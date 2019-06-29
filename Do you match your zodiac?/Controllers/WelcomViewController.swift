f79f84020d8a4755a757233233c0e32eacdedfd3
Sergey Kosilov <sergey.cosilov@yandex.ru>
2019-06-29 19:26:00 +0300

    b

--- Do you match your zodiac?/Controllers/WelcomViewController.swift
+++ Do you match your zodiac?/Controllers/WelcomViewController.swift
@@ -9,14 +9,27 @@
 import UIKit
 
 class WelcomViewController: UIViewController {
-
+    
+    var nameImage = ["Aquarius", "Aries", "Cancer", "Capricorn", "Gemini", "Leo", "Libra", "Pisces", "Sagittarius", "Scorpion", "Taurus", "Virgo"]
+    
+    
+    @IBOutlet var zodiacImage: [UIImageView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateZodiacImage(image: zodiacImage)
        
     }
     
 
-   
+    func updateZodiacImage(image: [UIImageView]){
+        
+        let randomIndex = Int(arc4random_uniform(UInt32(nameImage.count)))
+        for images in zodiacImage{
+            images.image = UIImage(named: nameImage[randomIndex])
+            nameImage.remove(at: randomIndex)
+            
+        }
+    }
 
 }
